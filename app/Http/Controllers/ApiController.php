46354cf6ecba355daf9766c6acb5943d7964d9b1
FebriTahta <febrirtah@gmail.com>
2023-11-17 00:07:56 +0700

    revamp query wherenotin api daftar berita

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -124,7 +124,7 @@ class ApiController extends Controller
     {
         $data = News::orderBy('id','desc')->whereHas('kategori',function($query){
             $query->whereNotIn('kategori_slug', ['prestasi', 'program-unggulan']);
-        })->with('kategori')->paginate(10);
+        })->paginate(10);
         if ($data) {
             # code...
             return ApiFormatter::createApi(200, 'success' ,$data);
